d39d70d434bcf1428cc0fe4c087d9518d201655a
aspark003 <trust05171980@gamil.com>
2025-09-19 17:28:28 -0400

    final deployment setup

--- deploy_model.py
+++
@@ -1,9 +0,0 @@
-import requests
-import gunicorn
-
-url = "http://127.0.0.1:5000/model_prediction"
-payload = {"features": [1.293665, 17.877919]}  # use real feature values
-
-response = requests.post(url, json=payload)
-print(response.json())
-
